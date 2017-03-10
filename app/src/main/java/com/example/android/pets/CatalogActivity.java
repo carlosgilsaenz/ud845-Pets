a8a98a0255fc9e8edd06f8eaca9683840adceb2e
Saenz <carlos.saenz@stantec.com>
2017-03-09 20:36:29 -0800

    1.4 Add insertPet() for dummy data

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.pets;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -90,6 +91,28 @@ public class CatalogActivity extends AppCompatActivity {
         }
     }
 
+    private void insertPet(){
+        // To access our database, we instantiate our subclass of SQLiteOpenHelper
+        // and pass the context, which is the current activity.
+        PetDbHelper mDbHelper = new PetDbHelper(this);
+
+        // Gets the data repository in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        // Create a new map of values, where column names are the keys
+        ContentValues values = new ContentValues();
+        values.put(petEntry.COLUMN_PET_NAME, "Toto");
+        values.put(petEntry.COLUMN_PET_BREED, "Terrier");
+        values.put(petEntry.COLUMN_PET_GENDER, 1);
+        values.put(petEntry.COLUMN_PET_WEIGHT, 7);
+
+        // Insert the new row, returning the primary key value of the new row
+        long newRowId = db.insert(petEntry.TABLE_NAME, null, values);
+
+        //checks if insert was successful and updates screen
+        if(newRowId != -1){displayDatabaseInfo();}
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu options from the res/menu/menu_catalog.xml file.
@@ -104,7 +127,7 @@ public class CatalogActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             // Respond to a click on the "Insert dummy data" menu option
             case R.id.action_insert_dummy_data:
-                // Do nothing for now
+                insertPet();
                 return true;
             // Respond to a click on the "Delete all entries" menu option
             case R.id.action_delete_all_entries:
